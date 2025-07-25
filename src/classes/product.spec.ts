03e0f19df07b9daec364b5f69a396e2d2058b657
Luciano Machado <lucianoo_machado@hotmail.com>
2025-07-25 08:44:09 -0300

    testing product.ts

---
+++ src/classes/product.spec.ts
@@ -0,0 +1,16 @@
+import { Product } from "./product";
+
+const createSut = (name: string, price: number): Product => {
+  return new Product(name, price);
+}
+
+describe('Messaging', () => {
+  afterEach(() => jest.clearAllMocks())
+
+  it('should return undefined', () => {
+    const sut = createSut('Camiseta', 49.9);
+    expect(sut).toHaveProperty('name', 'Camiseta');//Testando propriedades
+    expect(sut.price).toBeCloseTo(49.9);// Para testar numeros flutuantes, passamos o parametro como metodo no argumento e fazemos a verificação com o toBeCloseTo
+  })
+
+})
--- src/classes/product.ts
+++ src/classes/product.ts
@@ -1,3 +1,5 @@
+import { CardItem } from "./interfaces/cart-item";
+
 export class Product implements CardItem {
   constructor(
     public name: string,
