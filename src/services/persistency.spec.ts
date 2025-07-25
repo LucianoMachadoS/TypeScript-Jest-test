3c07c0a1bfdae68aba69dfd38b4d491fa4b4cbac
Luciano Machado <lucianoo_machado@hotmail.com>
2025-07-25 08:19:51 -0300

    First test with spyOn

--- src/services/persistency.spec.ts
+++ src/services/persistency.spec.ts
@@ -1,14 +1,26 @@
+import { Persistency } from "./persistency";
 
-describe('TESTANDO ALGUMA COISA', ()=>{
-  it('descrição do teste (IT)', () => {
-    const number = 1;
-    expect(number).toBe(1);
+
+describe('Persistency', () => {
+  afterEach(() => jest.clearAllMocks())
+
+  it('should return undefined', () => {
+    // System under test
+    const sut = new Persistency();
+    expect(sut.saveOrder()).toBeUndefined();
+  })
+
+  it('should call console.log once', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder()
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
   });
-})
 
-describe('TESTANDO OUTRA COISA', () =>{
-  test('descrição de teste(TESTE)', () => {
-    const nome = 'Luciano';
-    expect(nome).toBe('Luciano');
+  it('should call console.log once with "Pedido salvo com sucesso."', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder()
+    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso.');
   });
 })
