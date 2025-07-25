d371f35d320d8ac466db4592dec27d469652ae1c
Luciano Machado <lucianoo_machado@hotmail.com>
2025-07-25 18:16:53 -0300

    testing discount

--- src/classes/product.spec.ts
+++ src/classes/product.spec.ts
@@ -4,10 +4,10 @@ const createSut = (name: string, price: number): Product => {
   return new Product(name, price);
 }
 
-describe('Messaging', () => {
+describe('Product', () => {
   afterEach(() => jest.clearAllMocks())
 
-  it('should return undefined', () => {
+  it('should have properties name and price', () => {
     const sut = createSut('Camiseta', 49.9);
     expect(sut).toHaveProperty('name', 'Camiseta');//Testando propriedades
     expect(sut.price).toBeCloseTo(49.9);// Para testar numeros flutuantes, passamos o parametro como metodo no argumento e fazemos a verificação com o toBeCloseTo
